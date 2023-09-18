f52c51c74a1a15e8b1718fe9afecdadf8a95ec49
ldunko <zsefvcx@list.ru>
2023-09-19 00:01:04 +0300

    lesson21 25%+

--- categories_sql_lite/lib/presentation/groups.dart
+++ categories_sql_lite/lib/presentation/groups.dart
@@ -105,7 +105,10 @@ class _GroupsPageState extends State<GroupsPage> {
                     //Logger.print('${Categories.instance().group}', name: 'log', level: 0, error: false);
                   },
                   tooltip: 'Add a new Group',
-                  child: const Icon(Icons.add),
+                  child: const Hero(
+                      tag: 'animateHero',
+                      child: Icon(Icons.add),
+                  ),
                 ),
                 const SizedBox(height: 10,),
                 FloatingActionButton(
@@ -125,127 +128,130 @@ class _GroupsPageState extends State<GroupsPage> {
     );
 
   }
-
+//https://stackoverflow.com/questions/44403417/hero-animation-with-an-alertdialog
   Future<void> _dialogBuilder(BuildContext context) {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Add a new Group'),
-          content: SizedBox(
-            width: 300,
-            height: 300,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  SizedBox(
-                    width: 250,
-                    child: TextFormField(
-                      controller: _group,
-                      maxLines: 1,
-                      maxLength: 10,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Group Name',
+        return Hero(
+          tag: 'animateHero',
+          child: AlertDialog(
+            title: const Text('Add a new Group'),
+            content: SizedBox(
+              width: 300,
+              height: 300,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 250,
+                      child: TextFormField(
+                        controller: _group,
+                        maxLines: 1,
+                        maxLength: 10,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Group Name',
+                        ),
+                        validator: (text) =>
+                            (text == null || text.isEmpty)?
+                            'Text is empty':
+                            null,
                       ),
-                      validator: (text) =>
-                          (text == null || text.isEmpty)?
-                          'Text is empty':
-                          null,
                     ),
-                  ),
-                  const Divider(height: 20,),
-                  SizedBox(
-                    width: 250,
-                    child: TextFormField(
-                      controller: _image,
-                      maxLength: 255,
-                      maxLines: 1,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Image URL',
-                      ),
-                      validator: (value) {
-                        ///Используем встроенный валидатор
-                        if(value != null){
-                          final Uri? uri = Uri.tryParse(value);
-                          if(uri != null) {
-                            if (!uri.hasAbsolutePath) {
-                              return 'Please enter valid url';
-                            } else {
-                              return null;
+                    const Divider(height: 20,),
+                    SizedBox(
+                      width: 250,
+                      child: TextFormField(
+                        controller: _image,
+                        maxLength: 255,
+                        maxLines: 1,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Image URL',
+                        ),
+                        validator: (value) {
+                          ///Используем встроенный валидатор
+                          if(value != null){
+                            final Uri? uri = Uri.tryParse(value);
+                            if(uri != null) {
+                              if (!uri.hasAbsolutePath) {
+                                return 'Please enter valid url';
+                              } else {
+                                return null;
+                              }
                             }
                           }
-                        }
-                        return 'Please enter valid url';
-                        ///Используем регулярные выражения
-                        // String hasValidUrl(String value) {
-                        //   String pattern = r'(http|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?';
-                        //   RegExp regExp = new RegExp(pattern);
-                        //   if (value.length == 0) {
-                        //     return 'Please enter url';
-                        //   }
-                        //   else if (!regExp.hasMatch(value)) {
-                        //     return 'Please enter valid url';
-                        //   }
-                        //   return null;
-                        // }
-                      },
+                          return 'Please enter valid url';
+                          ///Используем регулярные выражения
+                          // String hasValidUrl(String value) {
+                          //   String pattern = r'(http|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?';
+                          //   RegExp regExp = new RegExp(pattern);
+                          //   if (value.length == 0) {
+                          //     return 'Please enter url';
+                          //   }
+                          //   else if (!regExp.hasMatch(value)) {
+                          //     return 'Please enter valid url';
+                          //   }
+                          //   return null;
+                          // }
+                        },
+                      ),
                     ),
-                  ),
-                  const Divider(height: 20,),
-                  Expanded(
-                    child: Container(
-                      constraints: const BoxConstraints(maxHeight: 200),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        reverse: false,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
-                            controller: _description,
-                            keyboardType: TextInputType.multiline,
-                            maxLines: 1, //grow automatically
-                            maxLength: 40,
-                            minLines: 1,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Description',
+                    const Divider(height: 20,),
+                    Expanded(
+                      child: Container(
+                        constraints: const BoxConstraints(maxHeight: 200),
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.vertical,
+                          reverse: false,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              controller: _description,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 1, //grow automatically
+                              maxLength: 40,
+                              minLines: 1,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Description',
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              style: TextButton.styleFrom(
-                textStyle: Theme.of(context).textTheme.labelLarge,
-              ),
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                textStyle: Theme.of(context).textTheme.labelLarge,
-              ),
-              child: const Text('Add'),
-              onPressed: () async {
-                var cSt = _formKey.currentState;
-                if(cSt != null && cSt.validate()){
-                  addData();
+            actions: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                child: const Text('Cancel'),
+                onPressed: () {
                   Navigator.of(context).pop();
-                }
-              },
-            ),
-          ],
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                child: const Text('Add'),
+                onPressed: () async {
+                  var cSt = _formKey.currentState;
+                  if(cSt != null && cSt.validate()){
+                    addData();
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+            ],
+          ),
         );
       },
     );
